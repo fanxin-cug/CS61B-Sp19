0271f268f239b6c81e01870bd88135b93e030bc7
fanxin <fanxin806@cug.edu.cn>
2021-03-28 16:53:33 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        //System.out.println(5 + "10");
+        int x = 0,ans=0;;
+        while (x < 10) {
+            ans=ans+x;
+            System.out.print(ans + " ");
+            x = x + 1;
+        }
+    }
+}
