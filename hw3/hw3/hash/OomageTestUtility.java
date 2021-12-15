2e016edfe42f9e0ab28456c9a4b92320e48829f4
fanxin <fanxin806@cug.edu.cn>
2021-12-15 12:35:13 +0800

    finished hw3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -12,6 +12,17 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        int[] num = new int[M];
+        for(Oomage o : oomages){
+            int bucketNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            num[bucketNum]+=1;
+        }
+        int N=oomages.size();
+        for (int i : num) {
+            if (i < N / 50.0 || i > N / 2.5) {
+                return false;
+            }
+        }
+        return true;
     }
 }
